e52a612d8b21692c3a45c13183281868a457bb4b
Seokwoo_Kang <ksukwoo91@gmail.com>
2023-01-02 21:21:16 +0900

    update

---
+++ Day060(Dec.27)_Datahandling17/Sigmoid 시각화/ex05.py
@@ -0,0 +1,61 @@
+# 파이토치 클래스로 로지스틱 회귀를 구현하기
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+# 1. x_data, y_data new
+x_data = [[1,2], [2,3], [3,1], [4,3], [5,3], [6,2]]
+y_data = [[0], [0], [0], [1], [1], [1]]
+
+# 2. x_data, y_data -> tensor
+x_train = torch.FloatTensor(x_data)
+y_train = torch.FloatTensor(y_data)
+
+class BinaryClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(2,1)    # input dim = 2, output dim =1
+        self.sigmoid = nn.Sigmoid()     # 출력은 시그모이드 함수를 거칩니다.
+
+    def forward(self, x):
+        return self.sigmoid(self.linear(x))
+
+# 3. 모델 호출
+model = BinaryClassifier()
+print(model)
+"""
+BinaryClassifier(
+  (linear): Linear(in_features=2, out_features=1, bias=True)
+  (sigmoid): Sigmoid()
+)
+"""
+
+# 4. optimizer 설정
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+
+# 5. 얼마만큼 반복 할껀지
+epoch_num = 1000
+
+# 6. 학습 진행
+for epoch in range(epoch_num+1):
+
+    output = model(x_train)
+
+    # loss
+    loss = F.binary_cross_entropy(output, y_train)
+
+    optimizer.zero_grad() # gradient를 None으로 설정
+    loss.backward()
+    optimizer.step()    # 단일 optimization step을 수행하고 parameter 업데이트
+
+    if epoch%10==0:
+        prediction = output >= torch.FloatTensor([0.5])
+        correct_prediction = prediction.float() == y_train
+        acc = correct_prediction.sum().item() / len(correct_prediction)
+        print("Epoch : {:4d}/{} loss :{:.6f} acc : {:.2f}%".format(
+            epoch, epoch_num, loss.item(), acc * 100
+        ))
+    
+print(model(x_train))
+    
